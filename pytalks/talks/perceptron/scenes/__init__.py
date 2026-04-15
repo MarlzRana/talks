06106f9db46871a6fad80dd3cf31a418d55c24d0
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2026-04-16 00:45:26 +0100

    Pytalks:feat: Go Back Scenes

--- pytalks/talks/perceptron/scenes/__init__.py
+++ pytalks/talks/perceptron/scenes/__init__.py
@@ -1,4 +1,5 @@
 from manimlib import *
+from pyglet.window import key as PygletKeys
 
 from talks.perceptron.scenes.s01_title import slide_title
 from talks.perceptron.scenes.s02_single_perceptron import slide_single_perceptron
@@ -17,13 +18,50 @@ SLIDES = [
 ]
 
 
+class GoBack(Exception):
+    pass
+
+
 class PerceptronTalk(Scene):
+    def setup(self):
+        self._go_back = False
+        self._wait_count = 0
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == PygletKeys.LEFT:
+            self._go_back = True
+            self.hold_on_wait = False
+            return
+        super().on_key_press(symbol, modifiers)
+
+    def wait(self, *args, **kwargs):
+        self._wait_count += 1
+        super().wait(*args, **kwargs)
+        if self._go_back:
+            raise GoBack()
+
     def construct(self):
-        for i, slide_fn in enumerate(SLIDES):
-            slide_fn(self)
-            # Clear all objects before the next slide (except the last)
+        i = 0
+        while i < len(SLIDES):
+            self._go_back = False
+            self._wait_count = 0
+            try:
+                SLIDES[i](self)
+            except GoBack:
+                pass
+
+            if self._go_back:
+                self.clear()
+                if self._wait_count <= 1:
+                    # At the beginning of the slide — go to previous
+                    i = max(0, i - 1)
+                # Otherwise just restart current slide (i stays the same)
+                continue
+
+            # Clear before next slide
             if i < len(SLIDES) - 1:
                 self.play(
                     *[FadeOut(mob) for mob in self.mobjects],
                     run_time=0.5,
                 )
+            i += 1
